4157706d5075ac85d8190eddb289afd5e632d856
Manoranjanhere <manoranjanheremano@gmail.com>
2025-03-09 01:24:02 +0530

    urls changed

--- backend/controllers/videoController.js
+++ backend/controllers/videoController.js
@@ -244,7 +244,7 @@ console.log('9. Storing transcript in ChromaDB...');
 try {
   // Initialize ChromaDB client with explicit server URL
   const chromaClient = new ChromaDB.ChromaClient({
-    path: "http://localhost:8000" // Make sure this points to your running ChromaDB server
+    path: process.env.CHROMA_URL ||"http://localhost:8000" // Make sure this points to your running ChromaDB server
   });
   
   // Define collection name based on user ID
--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,9 @@ const app = express();
 
 //middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials: true
-  }));
+  origin: process.env.FRONTEND_URL || 'http://64.227.152.247:5173',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
