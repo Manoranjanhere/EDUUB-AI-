61d88ce9e8b5f73ad18e747113cb7997a13cc899
Manoranjanhere <manoranjanheremano@gmail.com>
2025-03-16 12:45:12 +0530

    handling cors issue

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,9 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Authorization', 'Content-Type', 'Accept', 'Origin', 'X-Requested-With']
 }));
+
+app.options('*', cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
