fd2ec2ad6a2ae06885926c0d8917963574a6cccf
manoranjanhere <manoranjanheremano@gmail.com>
2025-05-19 02:46:15 +0530

    api changed

--- backend/controllers/qaController.js
+++ backend/controllers/qaController.js
@@ -7,7 +7,7 @@ import * as ChromaDB from 'chromadb'; // Correct import
 dotenv.config();
 
 const groq = new Groq({ 
-  apiKey: process.env.GROQ_API_KEY || "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" 
+  apiKey: process.env.GROQ_API_KEY || "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" 
 });
 
 // At the top of your file, add this variable to track speech state
